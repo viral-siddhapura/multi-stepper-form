61227eac9c8adffe76566dbb4d97767755d51856
HVMS <viralsid2330@gmail.com>
2024-09-09 00:15:23 -0300

    authorization, pages, session strategies added and data validation for login part added

---
+++ multi-stepper-form/server/auth.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "./db"
+import Credentials from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
+import { eq } from "drizzle-orm"
+import { LoginSchema } from "@/types/login-schema"
+import { users } from "./schema"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: DrizzleAdapter(db),
+    providers: [
+        Credentials({
+            authorize: async (credentials) => {
+                const validatedFields = LoginSchema.safeParse(credentials);
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+
+                    const user = await db.query.users.findFirst({
+                        where: eq(users.email, email),
+                    });
+
+                    if (!user || !user.password) {
+                        return null;
+                    }
+
+                    const passwordMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordMatch) {
+                        return user;
+                    }
+                }
+
+                return null;
+            }
+        })
+    ],
+    pages: {
+        signIn: "/login",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    }
+})
