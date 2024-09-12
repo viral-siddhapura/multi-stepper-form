118e2e095a13378f892739c2fbd2c30facca54e1
HVMS <viralsid2330@gmail.com>
2024-09-12 01:31:54 -0300

    files updated

--- multi-stepper-form/components/auth/multi-step-form/step-one.tsx
+++ multi-stepper-form/components/auth/multi-step-form/step-one.tsx
@@ -6,7 +6,6 @@ import { z } from "zod";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FormProvider } from "react-hook-form";
 
 type StepOneProps = {
     onNext: (values: z.infer<typeof firstStepSchema>) => void;
@@ -35,84 +34,82 @@ const StepOne = ({ onNext }: StepOneProps) => {
             <p className="text-gray-500 text-sm mt-3">👏🏻 Let&apos;s start with a little bit of information</p>
 
             <div className="mt-10">
-                <FormProvider>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                        >
-                            <div className="grid grid-cols-2 gap-10 mb-5">
-                                <FormField
-                                    control={form.control}
-                                    name="firstName"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>First Name:</FormLabel>
-                                            <FormControl>
-                                                <Input
-                                                    {...field}
-                                                    placeholder="Enter first name"
-                                                    type="text"
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="lastName"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Last Name:</FormLabel>
-                                            <FormControl>
-                                                <Input
-                                                    {...field}
-                                                    placeholder="Enter last name"
-                                                    type="text"
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                    >
+                        <div className="grid grid-cols-2 gap-10 mb-5">
                             <FormField
                                 control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem className="mb-5">
-                                        <FormLabel>Email Address:</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                {...field}
-                                                placeholder="Enter your email"
-                                                type="email"
-                                            />
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="location"
+                                name="firstName"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel>Location:</FormLabel>
+                                        <FormLabel>First Name:</FormLabel>
                                         <FormControl>
                                             <Input
                                                 {...field}
-                                                placeholder="Enter your location"
+                                                placeholder="Enter first name"
                                                 type="text"
                                             />
                                         </FormControl>
                                     </FormItem>
                                 )}
                             />
-                            <Button
-                                className="mt-5 w-full"
-                                type="submit"
-                            >Continue</Button>
-                        </form>
-                    </Form>
-                </FormProvider>
+                            <FormField
+                                control={form.control}
+                                name="lastName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Last Name:</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="Enter last name"
+                                                type="text"
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem className="mb-5">
+                                    <FormLabel>Email Address:</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Enter your email"
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="location"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Location:</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Enter your location"
+                                            type="text"
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            className="mt-5 w-full"
+                            type="submit"
+                        >Continue</Button>
+                    </form>
+                </Form>
             </div>
         </div>
     )
