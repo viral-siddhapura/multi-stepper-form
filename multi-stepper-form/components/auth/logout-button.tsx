0bb6ceab07b88a2030ed26930584b61f53b6351b
HVMS <viralsid2330@gmail.com>
2024-09-13 12:03:12 -0400

    logout button created

---
+++ multi-stepper-form/components/auth/logout-button.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Button } from "../ui/button";
+import { signOut } from "next-auth/react";
+
+export default function LogoutButton() {
+    return (
+        <Button onClick={() => signOut()}>Logout</Button>
+    )
+}
