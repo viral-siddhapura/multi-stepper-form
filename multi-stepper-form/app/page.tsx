162edd87ee0c8f1405744ecff3d11952cc18763d
HVMS <viralsid2330@gmail.com>
2024-09-13 12:02:04 -0400

    home section added

--- multi-stepper-form/app/page.tsx
+++ multi-stepper-form/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import LogoutButton from "@/components/auth/logout-button";
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div>
-      Hello world
+      <h1>Authenticated</h1>
+      <LogoutButton />
     </div>
   );
 }
